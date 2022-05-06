bfcc884dcc17f059842d74a7622f3fa29068284b
Oybek <oybekjonxayrullayevich@gmail.com>
2022-05-06 10:55:14 +0500

    start

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function App(){
+
+  return (
+    <div className="App">
+<Board></Board>
+    </div>
+  )
+}
+function Board(){
+return (
+  <div className="Board">
+<Block></Block>
+  </div>
+)
+}
+function Block(){
+  return (
+  <div className="Block">
+    
+  </div>
+)
+}
